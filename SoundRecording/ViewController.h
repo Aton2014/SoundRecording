43bc761cdf257efd6e6de08fdef366cee140386b
aton <aton_zhang_yd@163.com>
2016-09-21 18:15:24 +0800

    Initial Commit

---
+++ SoundRecording/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundRecording
+//
+//  Created by aton on 16/9/21.
+//  Copyright © 2016年 aton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SoundRecording/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SoundRecording
+//
+//  Created by aton on 16/9/21.
+//  Copyright © 2016年 aton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
